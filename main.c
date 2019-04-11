2fdc1ee89a2ca47849ec5af25bb6fc5c98fa92c8
RxnNode <43140306+RxnNode@users.noreply.github.com>
2019-04-11 10:35:32 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,39 +1,41 @@
 #include <stdio.h>
-
 int main() {
-    printf("=================================\n");
-    printf("Vigenere_Cipher\n");
-    printf("1. Encryption 2. Decryption\n");
-    printf("=================================\n");
-
-    int a = 0;
-    char c;
-
-    while((c = getchar()) != EOF){
-        char messbuff[256];
-        char keybuff[256];
-        char cybuff[256];
-        if ( c == '1'){
-            fflush(stdin);
-            printf("Please enter message:");
-            fgets(messbuff, sizeof(messbuff), stdin);
-            printf("your: %s\n", messbuff);
-            //fflush(stdin);
-            printf("Please enter key;");
-            //fgets(keybuff, sizeof(keybuff), stdin);
-            fflush(stdin);
-            fscanf(stdin, " %s", keybuff);
-            printf("your: %s\n", keybuff);
-
-
-
-
-        } else if ( c == '2'){
-
-        } else{
-            printf("1 or 2\n");
+        char messbuff[] = "THIS IS A TEST";
+        char keybuff[] = "KEY";
+        char cybuff[256] = "";
+        char decbuff[256] = "";
+        unsigned int ml =  sizeof(messbuff) /sizeof(messbuff[0]);
+        unsigned int kc = 0 ,kl = sizeof(keybuff) / sizeof(keybuff[0]);
+        for (int i = 0; i < ml - 1; i++) {
+            if ( messbuff[i] != ' ') {
+                if (kc < kl -1) {
+                    cybuff[i] = ((messbuff[i] + keybuff[kc]) % 26) + 'A';
+                } else {
+                    kc = 0;
+                    cybuff[i] = ((messbuff[i] + keybuff[kc]) % 26) + 'A';
+                }
+                kc++;
+            } else{
+                cybuff[i] = ' ';
+            }
         }
-    }
-
+        kc = 0;
+        for (int j = 0; j <  ml - 1; ++j) {
+            if ( cybuff[j] != ' ') {
+                if (kc < kl -1) {
+                    decbuff[j] = ((cybuff[j] - keybuff[kc] + 26)) % 26 + 'A';
+                    printf("%c %c %c\n",messbuff[j],keybuff[kc],decbuff[j]);
+                } else {
+                    kc = 0;
+                    decbuff[j] = ((cybuff[j] - keybuff[kc] + 26)) % 26 + 'A';
+                    printf("*%c %c %c\n",messbuff[j],keybuff[kc],decbuff[j]);
+                }
+                kc++;
+            } else{
+                decbuff[j] = ' ';
+            }
+        }
+        printf("Enc: %s\n",cybuff);
+        printf("Dec: %s", decbuff);
     return 0;
 }
