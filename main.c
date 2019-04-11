ac6ac933c733c2a9535712e8ace4f30b0ea58b73
RxnNode <43140306+RxnNode@users.noreply.github.com>
2019-04-11 09:23:31 +0800

    Update main.c

--- main.c
+++ main.c
@@ -5,5 +5,35 @@ int main() {
     printf("Vigenere_Cipher\n");
     printf("1. Encryption 2. Decryption\n");
     printf("=================================\n");
+
+    int a = 0;
+    char c;
+
+    while((c = getchar()) != EOF){
+        char messbuff[256];
+        char keybuff[256];
+        char cybuff[256];
+        if ( c == '1'){
+            fflush(stdin);
+            printf("Please enter message:");
+            fgets(messbuff, sizeof(messbuff), stdin);
+            printf("your: %s\n", messbuff);
+            //fflush(stdin);
+            printf("Please enter key;");
+            //fgets(keybuff, sizeof(keybuff), stdin);
+            fflush(stdin);
+            fscanf(stdin, " %s", keybuff);
+            printf("your: %s\n", keybuff);
+
+
+
+
+        } else if ( c == '2'){
+
+        } else{
+            printf("1 or 2\n");
+        }
+    }
+
     return 0;
 }
